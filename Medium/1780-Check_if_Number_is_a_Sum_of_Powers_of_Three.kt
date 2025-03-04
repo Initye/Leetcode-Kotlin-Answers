23b5770f87eeeb59ed79e8ac9f227e2cd5cb2c2e
Dekroo <dekroostream@gmail.com>
2025-03-04 12:52:07 +0100

    Create 1780-Check_if_Number_is_a_Sum_of_Powers_of_Three.kt

---
+++ Medium/1780-Check_if_Number_is_a_Sum_of_Powers_of_Three.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun checkPowersOfThree(n: Int): Boolean {
+        var p = n
+        val ternary = StringBuilder() //Create a String to hold ternary
+
+        while(p > 0) { 
+            val remainder = p % 3 //Get the remainder of dividing
+            ternary.append(remainder) //Store remainder
+            p /= 3 //Reduce the number by dividing by 3
+        }
+
+        for(i in 0 until ternary.length) { //Go through our ternary
+           if(ternary[i] > '1') { //If any number is above 1, f.e. 102 then its false, else true
+                return false
+           } 
+        }
+        return true
+    }
+}
